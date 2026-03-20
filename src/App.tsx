9a7cb7bc0d3f74d3e698f266c9ad0f2edd64208f
Lucas Brandi <lgbrandidantas17@gmail.com>
2026-03-20 14:58:26 -0300

    Aula 01

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './styles/global.css';
+import './styles/theme.css';
+import { Heading } from './components/Heading';
+
+function App() {
+  return (
+    <>
+      <Heading />
+    </>
+  );
+}
+
+export default App;
